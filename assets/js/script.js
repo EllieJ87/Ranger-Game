05fdbaf93e3a8de002e392471695bf9a5784fb76
EllieJ87 <82771820+EllieJ87@users.noreply.github.com>
2021-09-14 10:18:55 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,84 @@
+// selecting the modal div element
+//const modalElement = document.getElementById("modal")
+
+// selecing the modal text element
+const textModalElement = document.getElementById("modal-text")
+
+// selecting the main text element 
+const textElement = document.getElementById("text")
+
+// selecting the option buttons
+const optionButtonsElement = document.getElementById("option-buttons")
+
+// keep track of the character
+let state = {} // understand this point more
+
+// removes the opening modal when the user clicks on it
+function removeModal() {
+    // if the user clicks on the button the modal is removed
+    var myobj = document.getElementById("modal-box");
+    myobj.remove();    
+}
+
+// look at simplier game for a way to proceed to the options
+
+// show the options after the opening has been clicked
+/* function showOption() {
+    
+
+} */
+
+/*function myFuntion() {
+    var div = document.createElement("div");
+    div.innerHTML = "Hello";
+    document.body.appendChild(div);
+} */
+
+/* var mydiv = document.createElement("div");
+mydiv.id = "main";
+var button = document.createElement("button");
+button.textContent = "Click Me";
+button.id = "option-btn";
+mydiv.appendChild(button);
+var paragrapgh = document.createElement("p");
+paragrapgh.textContent = "Hello here";
+paragrapgh.id = "main-text";
+paragrapgh.classList.add("testing");
+mydiv.appendChild(paragrapgh);
+document.getElementsByTagName("body")[0].appendChild(mydiv); */
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Base from the Events html in the calculator application */
+
+// from the html select the element of the main text (div / p elements)
+// by declaring this as a const variable
+
+
+// selecting & declaring a const variable from the HTML element (buttons)
+
+// tracker to keep track of the character (let state = {} - research this element)
+
+// function to start the game state then goes to a node with the id ref of 1 to state where the game starts
+
+// function for the text nodes + what elements need to be removed or added 
+
+// showing the options / checking the state of the character
+
+// options that are being selected to proceed to the next question
+// by also checking the current state of the character
+
+// questions 
+
+// starts the game function
+
