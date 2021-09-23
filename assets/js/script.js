a4a24e1dbf9e63da73f85b9c47785738638a869e
EllieJ87 <82771820+EllieJ87@users.noreply.github.com>
2021-09-23 16:13:39 +0100

    feat added modal content and opening, and input field for the players name tx2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,84 +1,112 @@
-// selecting the modal div element
-//const modalElement = document.getElementById("modal")
+// https://codepen.io/rzim2082/pen/jrWYJW
 
-// selecing the modal text element
-const textModalElement = document.getElementById("modal-text")
+/* Psuedo Code example
+const updateRecords = (records) => {
+    // records arg is an object literal - properties are id (int), prop
+    ('tracks' or 'artist' - string)
+    // other logic goes here...
+};
+*/
 
-// selecting the main text element 
-const textElement = document.getElementById("text")
 
-// selecting the option buttons
-const optionButtonsElement = document.getElementById("option-buttons")
+// these variables connect our code with the 'id' on the html page
+// we can then manpulate the variables and it will manipulate the html
+const image = document.getElementById("images");
+const text = document.getElementById("text");
+const buttonBox = document.getElementById('buttonBox');
+const input = document.getElementById('input');
+const startBtn = document.getElementById('start-btn');
+
+// this is the variable for the player's name
+var playersName;
 
 // keep track of the character
-let state = {} // understand this point more
+//let state = {} // understand this point more
 
 // removes the opening modal when the user clicks on it
 function removeModal() {
+    console.log(input.value);
+    playersName = input.value;
     // if the user clicks on the button the modal is removed
     var myobj = document.getElementById("modal-box");
-    myobj.remove();    
-}
+    myobj.remove(); 
+    advanceTo(scenario.one)   
+} 
 
-// look at simplier game for a way to proceed to the options
+// this changes the text and puts in the players name
+var changeText = function(words) {
+    text.innerHTML = words.replace("Your name", playersName);
+};
 
-// show the options after the opening has been clicked
-/* function showOption() {
-    
+// this takes the image link and puts it in the proper format, sending it to the html
+var changeImage = function(img) {
+    image.style.backgroundImage = "<img src='" + img + "' alt='myimage'>";
+};
 
-} */
+// this looks at the numbe of options we have set and creates enough buttons
+var changeButtons = function(buttonList) {
+    buttonBox.innerHTML = "";
+    for (var i = 0; i < buttonList.length; i++) {
+        buttonBox.innerHTML += "<button onClick=" + buttonList[i][1] + ">" 
+        + buttonList[i][0] + "</button>";
+    };
+};
 
-/*function myFuntion() {
-    var div = document.createElement("div");
-    div.innerHTML = "Hello";
-    document.body.appendChild(div);
-} */
+// this is what moves the game along
+var advanceTo = function(s) {
+    changeImage(s.image)
+    changeText(s.text)
+    changeButtons(s.buttons)
+};
 
-/* var mydiv = document.createElement("div");
-mydiv.id = "main";
-var button = document.createElement("button");
-button.textContent = "Click Me";
-button.id = "option-btn";
-mydiv.appendChild(button);
-var paragrapgh = document.createElement("p");
-paragrapgh.textContent = "Hello here";
-paragrapgh.id = "main-text";
-paragrapgh.classList.add("testing");
-mydiv.appendChild(paragrapgh);
-document.getElementsByTagName("body")[0].appendChild(mydiv); */
+// this is the object that holds each scenario
+const scenario = {
+    one: {
+        image: 'assets/images/senku.jpg', 
+        text: "Your name yanks at the leash. You hear dogs barking and see an old abandoned house. Strangely, the door is wide open. What do you want to do?",
+        buttons: [
+            ["Turn and run", "advanceTo(scenario.three)"],
+            ["Enter The House", "advanceTo(scenario.four)"]
+        ]
+    },
+
+    two: {
+        image: 'assets/images/tanjiro.jpg',
+        text: "Your name yanks at the leash. You hear dogs barking and see an old abandoned house. Strangely, the door is wide open. What do you want to do?",
+        buttons: [
+            ["Turn and run", "advanceTo(scenario.three)"],
+            ["Enter The House", "advanceTo(scenario.four)"]
+        ]
+    },
+
+    three: {
+        image: 'assets/images/senku.jpg',
+        text: "Scenario 2",
+        buttons: [
+            ["continue", "advanceTo(scenario.four)"]
+        ]
+    },
+
+    four: {
+        image: 'assets/images/tanjiro.jpg',
+        text: "Scenario 3",
+        buttons: [
+            ["Follow your Dog Downstairs", "advanceTo(scenario.five)"],
+            ["Search the Kitchen for a knife", "advanceTo(scenario.five)"]
+        ]
+    },
+
+    five: {
+        image: 'assets/images/senku.jpg',
+        text: "TO BE CONTINUED...",
+    },
+
+};
 
 
 
+// this is the code that starts the game
+advanceTo(scenario.one);
 
 
-
-
-
-
-
-
-
-
-/* Base from the Events html in the calculator application */
-
-// from the html select the element of the main text (div / p elements)
-// by declaring this as a const variable
-
-
-// selecting & declaring a const variable from the HTML element (buttons)
-
-// tracker to keep track of the character (let state = {} - research this element)
-
-// function to start the game state then goes to a node with the id ref of 1 to state where the game starts
-
-// function for the text nodes + what elements need to be removed or added 
-
-// showing the options / checking the state of the character
-
-// options that are being selected to proceed to the next question
-// by also checking the current state of the character
-
-// questions 
-
-// starts the game function
 
